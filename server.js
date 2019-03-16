c0355ba30694ec63fbeefafe5aac1807ebea10a9
Jay <jkv5@psu.edu>
2019-03-16 18:05:17 -0400

    update json data

--- server.js
+++ server.js
@@ -10,9 +10,68 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(cors());
 
+<<<<<<< HEAD
+app.get('/jobs', function(req, res) {
+
+    res.json([ 
+    	{
+    		"company": 'geico', 
+    		"job-name": 'babysitter', 
+    		"description": 'do stuff', 
+    		"recruiter": 'bob smith'
+    	},
+
+    	{
+
+    	},
+
+
+
+    ]);
+});
+
+app.get('/companies', function(req, res) {
+
+    res.json([
+    	{
+    		"name": 'geico',
+    		"main-recruiter": 'bob smith',
+    	},
+    ]);
+
+});
+
+app.get('/employees', function(req, res) {
+
+    res.json([
+    	{
+    		"name": 'john porter',
+    		"gpa": 3.9,
+    		"address": ''.
+    		"phone": '',
+    		"bio": '',
+    		"major": '',
+    		"university": '',
+    		"github": '',
+    		"linkedin": ''
+    	}
+    ]);
+
+});
+
+
+app.get('/employers', function(req, res) {
+
+    res.json([
+    	{
+    		"success": true
+    	}
+    ]);
+=======
 app.get('/posts', function(req, res) {
 
     res.json({"success": true});
+>>>>>>> a58a6ef2ff2dfcf23b27c393269943e38f4e4f4b
 
 });
 

f02f05df496aecb38a97334ecf193224f28e2323
Jay <improcuziam@gmail.com>
2019-03-16 21:27:55 -0400

    restructure dir

--- node-server/server.js
+++ node-server/server.js
@@ -10,6 +10,17 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(cors());
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://root:<password>@dragonchatbot-mpbdy.gcp.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log(collection);
+  client.close();
+});
+
+
 app.get('/jobs', function(req, res) {
 
     res.json([ 
@@ -23,8 +34,6 @@ app.get('/jobs', function(req, res) {
     	{
 
     	},
-
-
 
     ]);
 });
@@ -46,7 +55,7 @@ app.get('/employees', function(req, res) {
     	{
     		"name": 'john porter',
     		"gpa": 3.9,
-    		"address": ''.
+    		"address": '',
     		"phone": '',
     		"bio": '',
     		"major": '',
